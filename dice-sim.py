a488b4cc3c136d8492c3ed385eb789ab14417515
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2022-07-08 21:25:56 +0530

    Create dice-sim.py
    Adding dice-sim.py
    A dice-simulation program.

---
+++ dice-sim.py
@@ -0,0 +1,10 @@
+import random
+
+again = 'y'
+while again:
+    print(f"The dice rolled {random.randint(1,6)}")
+    again = input("Want to continue? (y/n):\t")
+    
+    if again == 'y': continue 
+    else: break
+print("\nProgram terminated")
