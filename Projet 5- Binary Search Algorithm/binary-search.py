4bdb3c5108e8145297bad51866930fca6d5b0ea7
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2022-07-25 13:08:47 +0530

    updating binary search

--- Projet 5- Binary Search Algorithm/binary-search.py
+++ Projet 5- Binary Search Algorithm/binary-search.py
@@ -1,15 +1,12 @@
 def binary_search(arr, val):
-    if val < arr[0] or val > arr[-1]: return -1
-    else:
         end = len(arr) - 1
         start = 0
         mid = (end + start) // 2
 
-        while end != start:
+        while start <= end:
             if arr[mid] == val: return mid
             elif arr[mid] < val: start = mid + 1
-            else: end = mid
-
+            else: end = mid - 1
             mid = (end + start) // 2
-        
-        return mid
+            
+        return -1
