d792dcc80ae73cdf9544242edd5234e9a6501f89
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2023-09-02 23:05:21 +0530

    Update setup.py

--- Project 13- Random Walker/setup.py
+++ Project 13- Random Walker/setup.py
@@ -8,14 +8,15 @@ GREEN = (140, 235, 60)
 class Walker:
     def __init__(self) -> None:
         self.position = pygame.math.Vector2(SCREEN_DIM / 2, SCREEN_DIM / 2)
+        self.velocity = pygame.Vector2(random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0))
 
     def update_position(self) -> None:
         if not 0 < self.position.x < SCREEN_DIM or not 0 < self.position.y < SCREEN_DIM:
             self.position.x = SCREEN_DIM / 2
             self.position.y = SCREEN_DIM / 2
 
-        self.position.x += random.uniform(-1, 1)
-        self.position.y += random.uniform(-1, 1)
-    
+        self.position += self.velocity
+        self.velocity = pygame.Vector2(random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0))
+
     def show(self, screen: pygame.Surface) -> None:
         pygame.draw.circle(screen, GREEN, self.position, 1)
